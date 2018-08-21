0e5c7f0516e39b88b732a258b49308b9da3fc2f8
bytecore@gmail.com <mo10128>
2018-08-21 13:17:22 -0300

    Configurando layout do spinner

--- app/src/main/java/br/com/mojumob/crudcomsqlite/AddTarefaActivity.java
+++ app/src/main/java/br/com/mojumob/crudcomsqlite/AddTarefaActivity.java
@@ -2,9 +2,12 @@ package br.com.mojumob.crudcomsqlite;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 public class AddTarefaActivity extends AppCompatActivity {
 
@@ -19,6 +22,32 @@ public class AddTarefaActivity extends AppCompatActivity {
         setContentView(R.layout.activity_add_tarefa);
 
         //Inicializações
+        edtNomeTarefa      = findViewById(R.id.addTarefa_edtNomeTarefa);
+        edtDescricaoTarefa = findViewById(R.id.addTarefa_edtDescricaoTarefa);
+        spinnerStatus      = findViewById(R.id.addTarefa_spinnerStatus);
+        btnAcao            = findViewById(R.id.addTarefa_btnAcao);
 
+        configuraSpinner();
+
+
+        btnAcao.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Capturando o item selecionado do spinner
+                String x = spinnerStatus.getSelectedItem().toString();
+
+                Toast.makeText(AddTarefaActivity.this, x, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+    }
+
+    private void configuraSpinner() {
+        //Configurando o Spinner
+        ArrayAdapter adapterSpinner = ArrayAdapter.createFromResource(AddTarefaActivity.this,
+                R.array.status, R.layout.spinner_item);
+        adapterSpinner.setDropDownViewResource(R.layout.spinner_dropdown_item);
+        spinnerStatus.setAdapter(adapterSpinner);
     }
 }
